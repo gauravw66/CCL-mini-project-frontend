cb0541a3c74007929966c1fe8ffb076856c625d5
Gaurav Waghmare <135269754+gauravw66@users.noreply.github.com>
2025-04-17 17:42:43 +0530

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://ccl-mini-project-backend.onrender.com'
 });
 
 export const executeCode = async (code, language) => {
